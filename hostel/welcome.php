8428e01d5f99c979feeb16836f7f0ee57efe9da0
ANJANA WALAGAD <walagadanjana00@gmail.com>
2024-09-18 23:07:09 +0530

    commit

---
+++ hostel/welcome.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html>
+<head><title>Welcome</title>
+<style>
+body{
+background-image:url('img/hstl11.jpg');
+background-repeat:no-repeat;
+background-size:cover;
+background-position:center;
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+}
+
+.container{
+max-width:800px;
+padding:20px;
+background-color:#EAE7E2;
+border-radius:8px;
+box-shadow:0 0 20px rgba(0,0,0,0.3);
+text-align:center;
+}
+
+.t{
+background-color:#EAE7E2;
+padding:15px;
+max-width:500px;
+border:3px solid black ;
+border-radius:5px;
+position:absolute;
+
+bottom:0;
+right:0;
+}
+
+h1{
+color:black;
+}
+
+h3{
+color:black;
+}
+
+h2{
+bottom:0;
+left:0;
+}
+
+
+p{
+color:#555;
+margin-bottom:30px;
+}
+
+.btn {
+            display: inline-block;
+            padding: 10px 20px;
+            margin-top: 20px;
+            background-color: #007bff;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+        .btn:hover {
+            background-color: #0056b3;
+}
+
+</style>
+</head>
+
+<body ><center>
+<div class="container">
+<h1><strong>WELCOME TO HOSTEL MANAGEMENT SYSTEM </STRONG></h1>
+<form  method="post" action="login.php">
+<input class="btn" type ="submit" value="Get started"></form>
+
+</div>
+
+
+<div class="t"> 
+<h3>By</h3>
+<h3>Anjana Y Walagad (U03NM21T029005) </h3>
+<h3>Pranali Jadhav (U03NM21T029042) </h3></div>
+</body></html>
+
